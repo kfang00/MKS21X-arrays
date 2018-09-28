452a8e23100795487a5710568fff1c943bd26900
Kayla Fang <kfang00@stuy.edu>
2018-09-27 21:37:14 -0400

    finished 0b

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -6,12 +6,32 @@ class ArrayDemo {
 		}
 		System.out.println(printar + ary[ary.length - 1] + "]");
 	}
+	public static void printArray(int[][]ary){
+		String printar = "[";
+		for (int x = 0; x < ary.length; x++) {
+			printar += "[";
+			for (int y = 0; y < ary[x].length; y ++) {
+				printar += ary[x][y] + ",";
+			}
+			printar += ary[x][ary[x].length - 1] + "]";
+			if (x != ary.length - 1) {
+				printar += ",";
+			}
+		}
+		System.out.println(printar + "]");
+	}
 
 
 	public static void main (String[] args) {
-		int[] test0 = new int[] {
+		int[] test0a = new int[] {
 			1,2,3,4,5,6
 		};
-		printArray(test0);
+		int[][] test0b = new int[][] {
+			{2,4,5,3,2},
+			{2,5,2,6},
+			{8,6,5,4,6,7},
+		};
+		printArray(test0a);
+		printArray(test0b);
 	}
 }
